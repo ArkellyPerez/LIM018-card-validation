2510655d51e252aff0a241e46a2170c453036763
ArkellyPerez <arkelly.perez.alayo@gmail.com>
2022-05-23 03:11:33 -0500

    Valida el numero de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-//import validator from './validator.js';
+import validator from './validator.js';
 
-//console.log(validator);
+let number="";
 
 var buttonText=document.getElementById('button_validarr');
 buttonText.addEventListener("click",funcion_validar);
 
-//var numCantainer=document.getElementById('num_cantainer');
-//numCantainer.addEventListener("keydown",funcion_copiar);
-
-
-//const openModal = document.querySelector('.button_validar');
-//openModal.addEventListener('click', (e) => {
-  //  e.preventDefault();
-    //let name=document.querySelector('.num_tarjeta_container').value;
-    //document.querySelector('.rpta_validation').innerHTML = `tAJENTA ${name}`
-    //alert(name);
-  
- // })
-
-
 function funcion_validar(){
-  let number = document.querySelector('.num_tarjeta_container').value;
-  
-  alert(number);
+  number = document.querySelector('.num_tarjeta_container').value;
+   //alert(number);
+  validator.isValid(number);
+  if(validator.isValid(number)==true){
+  document.querySelector('.rpta_validation').innerHTML="Número de tarjeta correcto";
+  }
+  else {
+    document.querySelector('.rpta_validation').innerHTML="La Tarjeta ingresada no válida";
+    }
+//alert(number);
+ alert( validator.isValid(number));
+  //console.log(validator);
+
    }
+   
+ 
--- src/validator.js
+++ src/validator.js
@@ -1,12 +1,50 @@
-//const validator = {
-  // ...
-//};
+const validator = {
 
-//export default validator;
-var buttonText=document.getElementById('button_validarr');
-buttonText.addEventListener("click",funtion_validar);
+isValid:(card_number)=>{
+  let CardNumber=[...card_number].reverse();
+ // let numero1=Math.abs(CardNumber);
 
-function funtion_validar(){
-  alert('olii')
-  // ...
+let arrayPares = CardNumber.filter((num, index) => index % 2 !=0);
+let arrayImpares = CardNumber.filter((num, index) => index % 2 ==0);
+ // alert(arrayPares);
+  //alert(CardNumber);
+ let sumaNvoValorPar=0;
+ let sumaNvoValorImpar=0;
+ let flag=true;
+  for(let i in arrayPares){
+
+    let isNumber=Math.abs(arrayPares[i]);
+    let nvoValorPar=isNumber*2;
+    if(nvoValorPar>=10){
+      nvoValorPar=nvoValorPar-9;
+    }
+    sumaNvoValorPar=sumaNvoValorPar+nvoValorPar;
+  }
+
+  //alert( "el valor par es: "+sumaNvoValorPar);
+
+
+  for(let i in arrayImpares){
+    sumaNvoValorImpar=sumaNvoValorImpar+Math.abs(arrayImpares[i]);
+  }
+  //alert( "el valor impar es: "+sumaNvoValorImpar);
+
+  let sumatotal=sumaNvoValorImpar+sumaNvoValorPar;
+   if(sumatotal%10==0)
+    {
+      flag=true;
+    }
+    else {
+      flag=false;
+    }
+
+   // alert( "el valor total  es: "+sumatotal);
+
+
+
+return flag;
+
 }
+};
+
+export default validator;
