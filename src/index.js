1bb0c03dd96a418e5b32ee5f7938fd59d04bf919
ArkellyPerez <arkelly.perez.alayo@gmail.com>
2022-05-30 10:49:29 -0500

    card validaton version 2

--- src/index.js
+++ src/index.js
@@ -6,26 +6,21 @@ var buttonText=document.getElementById('button_validarr');
 buttonText.addEventListener("click",funcion_validar);
 
 function funcion_validar(){
-  number = document.querySelector('.num_tarjeta_container').value;
-   //alert(number);
-  validator.isValid(number);
+         number = document.querySelector('.num_tarjeta_container').value;
+          if(number==''){
+            document.querySelector('.rpta_validation').innerHTML="Por favor ingresar la informacion ";
+          }
+          else{
+              validator.isValid(number);
  
-  if(validator.isValid(number)==true){
-  document.querySelector('.rpta_validation').innerHTML="Número de tarjeta correcto";
-  }
-  else {
-    document.querySelector('.rpta_validation').innerHTML="La Tarjeta ingresada no es válida";
-    }
-
-  let str2=validator.maskify(number);
- // document.getElementById("num_cantainer").innerHTML=str2;
- document.querySelector('.rpta_encriptation').innerHTML="Tarjeta: "+str2;
-
- //alert( validator.isValid(number));
- console.log(str2);
-
-
-
-   }
-   
+              if(validator.isValid(number)==true){
+                  document.querySelector('.rpta_validation').innerHTML="Datos correctos";
+              }
+             else {
+                  document.querySelector('.rpta_validation').innerHTML="La Tarjeta ingresada no es válida";  
+             }
+             let str2=validator.maskify(number);
+             document.querySelector('.num_tarjeta_container').value=str2;
+          }  
+      }
  
