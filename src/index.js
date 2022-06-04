0660eee0de1e58332b673b25138ca207b0eec49c
ArkellyPerez <arkelly.perez.alayo@gmail.com>
2022-06-04 04:46:13 -0500

    Sumando el monto a pagar

--- src/index.js
+++ src/index.js
@@ -23,4 +23,35 @@ function funcion_validar(){
              document.querySelector('.num_tarjeta_container').value=str2;
           }  
       }
+     
+      let CheckboxCompra=document.getElementById('button_comprar');
+    CheckboxCompra.addEventListener('click',funtion_addCompra);
+    let montoCompra =0;
+    let name2=0;
+     // let ValorCompra=0;let ValorCompra=0;
+          
+      // let ValorCompra=0;let ValorCompra=0;
+ function funtion_addCompra(){
+   // alert("hol5");
+    for(let i=0; i<document.form.p1.length; i++){
+   //  console.log(i);
+        if(document.form.p1[i].checked){
+         //  alert("if");
+                montoCompra = document.form.p1[i].value;
+              //  alert("if2");
+               name2=parseInt(montoCompra,10)+name2;
+              // alert(name2);
+         }
+    } 
+   // alert(name2);
+    document.getElementById("MontoPagar").innerHTML="El monto total a pagar es: "+ name2+".00 Soles";   
+ }
+
+   //console.log("holiii");
+     // let ValorCompra=0;
+    //
  
+
+ // let buttonCompra=document.getElementById('button_comprar');
+   //   buttonCompra.addEventListener("click",funtion_comprar);
+  
